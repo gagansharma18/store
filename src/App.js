fdfd3c2e76708973830937bb5e92c2f33888cc7a
Sharma, Gagan <Gagan.Sharma@cdk.com>
2018-09-08 17:20:11 +0530

    Revert "Add home page and basic design"
    This reverts commit ca8b230a672322dbe50c2eca4836f18c797cf872.

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import Home from "./components/home";
-import reducers from "./reducers";
-const store = createStore(reducers, applyMiddleware(thunk));
-            <Provider store={store}>
-            </Provider>
